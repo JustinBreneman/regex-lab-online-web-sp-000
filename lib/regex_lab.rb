19867c936fc8dce2508fb47210cdfe611c02ecae
Justin Breneman <justin.breneman@yahoo.com>
2020-04-23 23:09:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\A\d(10)\z/) != nil
+  if phone.match(/\d{10}/) != nil
     true
 #  else
 #    false
