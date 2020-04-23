1aa04a719a51a6e1f065ad2108972261da2d641e
Justin Breneman <justin.breneman@yahoo.com>
2020-04-23 21:54:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[aeiou]+\w/) != nil
+  if word.match(/[aeiou]/) != nil
     true
-  elsif word.match(/[aeiou]+\w/) == nil
+  elsif word.match(/[aeiou]/) == nil
     false
   end
 end
